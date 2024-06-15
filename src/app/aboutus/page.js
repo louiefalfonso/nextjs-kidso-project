8bd969bbf26c3e97db36ec020c408a8e7a750221
Louie Alfonso <lfriasalfonso@gmail.com>
2024-06-15 21:00:44 +0100

    Kidso Kindergarten Baby Care Project

--- src/app/aboutus/page.js
+++ src/app/aboutus/page.js
@@ -2,14 +2,18 @@ import Layout from "@/components/layouts/Layout";
 import AboutArea from "@/components/sections/AboutArea";
 import BrandArea from "@/components/sections/BrandArea";
 import CounterArea from "@/components/sections/CounterArea";
+import TestimonialArea from "@/components/sections/TestimonialArea";
+import VideoArea from "@/components/sections/VideoArea";
 
 const AboutUs = () => {
   return (
     <>
       <Layout breadcrumbTitle="About Us">
-        <BrandArea />
         <AboutArea />
         <CounterArea />
+        <VideoArea />
+        <TestimonialArea />
+        <BrandArea />
       </Layout>
     </>
   );
